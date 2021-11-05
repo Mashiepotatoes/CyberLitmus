9acabf8b4119f3f95df5534be89b1312d536b4c7
mashiepotatoes <ashton.shane.dev@gmail.com>
2021-11-05 17:06:31 +0800

    improve interactivity w.r.t.details container and fix star rating

--- star-rating.js
+++ star-rating.js
@@ -1,7 +1,13 @@
-const stars = document.querySelectorAll('.fa-star')
+const stars = document.querySelectorAll('.fa-star');
 
 stars.forEach((star) => {
-  star.addEventListener('click', (event) => {
-    event.currentTarget.classList.toggle('yellow-star');
+  star.addEventListener('click', (e) => {
+    const clickedTarget = e.currentTarget;
+    stars.forEach(star => star.classList.remove('yellow-star')); // always reset to a blank slate on every click
+    const clickedStar = parseInt(clickedTarget.dataset.index, 10); // make dataset value into number
+
+    stars.forEach((star, index) => {
+      if (index <= clickedStar) { star.classList.add('yellow-star') }
+    });
   });
 });
