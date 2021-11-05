c8abb54a1dd8b450b53db7f9f171d7b8c747d051
mashiepotatoes <ashton.shane.dev@gmail.com>
2021-11-05 17:32:43 +0800

    test push - edit comment on line 7

--- star-rating.js
+++ star-rating.js
@@ -4,7 +4,7 @@ stars.forEach((star) => {
   star.addEventListener('click', (e) => {
     const clickedTarget = e.currentTarget;
     stars.forEach(star => star.classList.remove('yellow-star')); // always reset to a blank slate on every click
-    const clickedStar = parseInt(clickedTarget.dataset.index, 10); // make dataset value into number
+    const clickedStar = parseInt(clickedTarget.dataset.index, 10); // make dataset string value into number
 
     stars.forEach((star, index) => {
       if (index <= clickedStar) { star.classList.add('yellow-star') }
